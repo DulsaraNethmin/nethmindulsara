71934c83f53995052893fa68d0bbd956c55c6d8e
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-04-07 06:00:50 +0530

    stream blog

--- src/component/Blog/BlogApi.js
+++ src/component/Blog/BlogApi.js
@@ -1,6 +1,17 @@
 const BlogApi = [
   {
     id: 1,
+    image: "https://miro.medium.com/max/875/1*fObAmfTR55a_m_Ph-IpYyA.png",
+    date: "3 Feb, 2021",
+    link:"https://nethmindulsara.medium.com/flutter-streams-and-stream-builder-f267aa714673",
+    title_one: "Flutter Streams and StreamBuilder",
+    desc_one: "Nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.",
+    title_two: "Fundamentals of HTML CSS and JAVA SCRIPT.",
+    desc_two: "Mauris tempor, orci id pellentesque convallis, massa mi congue eros, sed posuere massa nunc quis dui. Integer ornare varius mi, in vehicula orci scelerisque sed. Fusce a massa nisi. Curabitur sit amet suscipit nisl. Sed eget nisl laoreet, suscipit enim nec, viverra eros. Nunc imperdiet risus leo, in rutrum erat dignissim id. Ut rhoncus vestibulum facilisis. Duis et lorem vitae ligula cursus venenatis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vitae nisi tortor. Morbi leo nulla, posuere vel lectus a, egestas posuere lacus. Fusce eleifend hendrerit bibendum. Morbi nec efficitur ex.",
+    title_three: "Mauris tempor, orci id pellentesque.",
+    desc_three: "Nulla non ligula vel nisi blandit egestas vel eget leo. Praesent fringilla dapibus dignissim. Pellentesque quis quam enim. Vestibulum ultrices, leo id suscipit efficitur, odio lorem rhoncus dolor, a facilisis neque mi ut ex. Quisque tempor urna a nisi pretium, a pretium massa tristique. Nullam in aliquam diam. Maecenas at nibh gravida, ornare eros non, commodo ligula. Sed efficitur sollicitudin auctor. Quisque nec imperdiet purus, in ornare odio. Quisque odio felis, vestibulum et.",
+  },{
+    id: 2,
     image: "https://miro.medium.com/max/1400/0*9fDrYHGECjYERpkT.png",
     date: "3 Feb, 2021",
     link:"https://baby-code-monk.medium.com/7-thing-you-should-know-as-a-web-developer-aac7b0d19b2",
@@ -12,7 +23,7 @@ const BlogApi = [
     desc_three: "Nulla non ligula vel nisi blandit egestas vel eget leo. Praesent fringilla dapibus dignissim. Pellentesque quis quam enim. Vestibulum ultrices, leo id suscipit efficitur, odio lorem rhoncus dolor, a facilisis neque mi ut ex. Quisque tempor urna a nisi pretium, a pretium massa tristique. Nullam in aliquam diam. Maecenas at nibh gravida, ornare eros non, commodo ligula. Sed efficitur sollicitudin auctor. Quisque nec imperdiet purus, in ornare odio. Quisque odio felis, vestibulum et.",
   },
   {
-    id: 2,
+    id: 3,
     image: "https://miro.medium.com/max/1400/0*sF9s4pBfgeKBz84N.jpeg",
     date: "23 Feb, 2021",
     link:"https://baby-code-monk.medium.com/all-about-array-methods-java-script-278153765dc8",
